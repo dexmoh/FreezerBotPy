44f55580df3dba7594e50af3d526e4a189b1be28
dexmoh <lazar.petrovic2310@gmail.com>
2024-07-11 14:07:21 +0200

    setup bot presence

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import console
 
 
 # Our bot's prefix (case sensitive).
@@ -14,3 +15,15 @@ bot = commands.Bot(command_prefix=prefix, intents=intents)
 
 # Flag we'll use to check whether the bot should use experimental features.
 experimental = False
+
+
+# This method runs when the bot connects for the first time.
+@bot.event
+async def on_ready():
+    console.log(f'Logged in as {bot.user} (ID: {bot.user.id}).')
+    await bot.change_presence(
+        activity=discord.Activity(
+            type=discord.ActivityType.listening,
+            name='WAAAAAHH'
+        )
+    )
