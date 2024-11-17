0b159a671d31217f293bc963ea0417c47c60cfc9
dexmoh <lazar.petrovic2310@gmail.com>
2024-11-17 12:41:01 +0100

    minor refactor

--- src/commands/chatbot.py
+++ src/commands/chatbot.py
@@ -8,13 +8,25 @@ from embed import create_embed
 @commands.command(name="fact", aliases=["facts"])
 async def fact(ctx):
     if ctx.message.guild.id not in ctx.bot.server_whitelist:
-        embed = create_embed(ctx, desc="This discord server doesn't have access to experimental features.", thumbnail_url=None)
-        await ctx.send(embed=embed)
+        await ctx.send(
+            embed=create_embed(
+                ctx,
+                desc="This discord server doesn't have access to experimental features.",
+                thumbnail_url=None,
+                set_footer=False
+            )
+        )
         return
 
     if not ctx.bot.experimental:
-        embed = create_embed(ctx, desc="Experimental features are currently disabled.", thumbnail_url=None)
-        await ctx.send(embed=embed)
+        await ctx.send(
+            embed=create_embed(
+                ctx,
+                desc="Experimental features are currently disabled.",
+                thumbnail_url=None,
+                set_footer=False
+            )
+        )
         return
 
     embed = create_embed(
